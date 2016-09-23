ea346eb0fb3b130ae537a85a571f6dd15c414bda
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2016-09-23 07:26:09 -0700

    Removed obsolete directories.

--- obsolete/init.el
+++
@@ -1,9 +0,0 @@
-(use-package geiser
-  :disabled t
-  :defer t
-  :config (require 'my-geiser))
-
-(use-package haskell-mode
-  :disabled t
-  :defer t
-  :config (require 'my-haskell))
--- obsolete/init_fancy_narrow.el
+++
@@ -1,8 +0,0 @@
-(eval-when-compile
-  (defvar fancy-narrow--beginning nil)
-  (defvar fancy-narrow--end nil)
-  (autoload 'fancy-narrow--motion-function "fancy-narrow" nil t))
-(use-package fancy-narrow
-  :disabled t
-  :commands (fancy-narrow-to-region fancy-widen))
-
