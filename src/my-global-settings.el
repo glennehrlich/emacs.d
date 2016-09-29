6c16eb587817fe8bbcf58cbc06a0d3c3b6878d24
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2016-09-29 09:10:53 -0700

    Use mode functions instead of setting mode variables.

--- src/my-global-settings.el
+++ src/my-global-settings.el
@@ -1,16 +1,16 @@
 ;;;; my-global-settings.el - Global settings.
 
 (customize-set-variable 'blink-cursor-blinks 0)
-(customize-set-variable 'column-number-mode t)
-(customize-set-variable 'delete-selection-mode t)
 (customize-set-variable 'enable-local-variables :all)
 (customize-set-variable 'indent-tabs-mode nil)
 (customize-set-variable 'inhibit-startup-screen t)
 (customize-set-variable 'initial-scratch-message nil)
-(customize-set-variable 'save-place-mode nil)
 (customize-set-variable 'split-height-threshold nil)
-(customize-set-variable 'tool-bar-mode nil)
-(customize-set-variable 'use-package-verbose t)
+
+(column-number-mode)
+(delete-selection-mode 1)
+(save-place-mode nil)
+(tool-bar-mode -1)
 
 (provide 'my-global-settings)
 
