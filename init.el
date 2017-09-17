877ab75c6328fbf559fa67fa1d88574b40e096a5
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-09-17 16:12:07 -0700

    Added my-dired.el, which turns on dired-dwim-target.
    dired-dwim-target set to t makes dired smarter about targets for
    copying, moving, etc.

--- init.el
+++ init.el
@@ -34,6 +34,10 @@
   :defer t
   :config (require 'my-compile))
 
+(use-package dired
+  :defer t
+  :config (require 'my-dired))
+
 (use-package ediff
   :defer t
   :config (require 'my-ediff))
---
+++ src/my-dired.el
@@ -0,0 +1,8 @@
+;;;; my-dired.el - Customizations for dired mode.
+
+(require 'dired)
+
+(customize-set-variable 'dired-dwim-target t)
+
+(provide 'my-dired)
+
