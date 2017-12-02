8b7175b9075350b75c3902605c7530598cc2b9b7
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-12-02 16:42:45 -0700

    bind C-f8 to counsel-rg (ripgrep) in my-ivy.el.

--- src/my-ivy.el
+++ src/my-ivy.el
@@ -21,6 +21,7 @@
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-x")     'counsel-M-x)
 (global-set-key (kbd "<f8>")    'counsel-ag)
+(global-set-key (kbd "<C-f8>")  'counsel-rg)
 
 (setq ivy-re-builders-alist
       '((counsel-M-x . ivy--regex-fuzzy)
