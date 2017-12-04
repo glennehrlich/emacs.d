3fff1da384fff283bebaf4a68b606c11959b6b96
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2017-12-04 07:09:38 -0700

    Set ring-bell-function to ignore.
    I tend to hit C-g (keyboard-quit) quite often and it rings the bell
    when C-g is hit at the top level instead of aborting a command in
    process, which I find annoying.

--- src/my-global-settings.el
+++ src/my-global-settings.el
@@ -3,6 +3,7 @@
 (column-number-mode)
 (delete-selection-mode 1)
 (tool-bar-mode -1)
+(customize-set-variable 'ring-bell-function 'ignore)
 
 ;; For some reason, save-place-mode is weird and this does not
 ;; work. I'm keeping '(save-place-mode nil) in custom.el for now.
