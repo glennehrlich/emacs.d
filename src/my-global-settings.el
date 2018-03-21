6b56ad2534bcce3d535f4878b0a3416855df2f7b
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-03-21 11:43:30 -0700

    Make sure blink cursor mode is always on.

--- src/my-global-settings.el
+++ src/my-global-settings.el
@@ -1,9 +1,10 @@
 ;;;; my-global-settings.el - Global settings.
 
+(blink-cursor-mode 1)
 (column-number-mode)
+(customize-set-variable 'ring-bell-function 'ignore)
 (delete-selection-mode 1)
 (tool-bar-mode -1)
-(customize-set-variable 'ring-bell-function 'ignore)
 
 ;; For some reason, save-place-mode is weird and this does not
 ;; work. I'm keeping '(save-place-mode nil) in custom.el for now.
