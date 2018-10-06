9d4064397b4626a37da1c1a06aee467f01e7aecc
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2018-10-06 11:06:11 -0700

    Switch keybindings of counsel-rg and counsel-ag in my-ivy.el.
    F8 now has counsel-rg (which I use more than counsel-ag).
    
    C-f8 now has counsel-ag.

--- src/my-ivy.el
+++ src/my-ivy.el
@@ -23,8 +23,8 @@
 (global-set-key (kbd "C-s")     'counsel-grep-or-swiper)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "M-x")     'counsel-M-x)
-(global-set-key (kbd "<f8>")    'counsel-ag)
-(global-set-key (kbd "<C-f8>")  'counsel-rg)
+(global-set-key (kbd "<f8>")    'counsel-rg)
+(global-set-key (kbd "<C-f8>")  'counsel-ag)
 
 (setq ivy-re-builders-alist
       '((counsel-M-x . ivy--regex-fuzzy)
