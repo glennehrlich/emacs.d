f7864339c8ca6b7c13968fce61000cefa4457a92
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-03-21 17:07:00 -0700

    Updated my-theme-looper.el to rename a function that got changed.

--- src/my-theme-looper.el
+++ src/my-theme-looper.el
@@ -2,14 +2,14 @@
 
 (require 'theme-looper)
 
-(theme-looper-set-theme-set '(
-                              base16-grayscale-dark ; first in list to cleanse palette
+(theme-looper-set-favorite-themes '(
+                                    base16-grayscale-dark ; first in list to cleanse palette
 
-                              base16-chalk
-                              idea-darkula
-                              niflheim
-                              noctilux
-                              zerodark
+                                    base16-chalk
+                                    idea-darkula
+                                    niflheim
+                                    noctilux
+                                    zerodark
                               ))
 
 (global-set-key (kbd "<M-f12>")   'theme-looper-enable-next-theme) ; Mac
