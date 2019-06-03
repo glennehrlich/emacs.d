a4727d86555a611372a1e2f7b5dc56b48a157088
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-06-03 11:16:53 -0700

    Make default theme be wombat while exploring themes.

--- custom.el
+++ custom.el
@@ -21,7 +21,7 @@
  '(counsel-find-file-at-point t)
  '(counsel-rg-base-command
    "rg --hidden --glob !.git -S --no-heading --line-number --color never %s .")
- '(custom-enabled-themes '(base16-chalk))
+ '(custom-enabled-themes '(wombat))
  '(custom-safe-themes t)
  '(default-frame-alist '((width . 100) (height . 60)))
  '(delete-old-versions t)
