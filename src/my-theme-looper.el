407c6f769c032a02a2ff9bed18d50b3fb124165b
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-06-03 13:16:14 -0700

    Make my-theme-looper reflect current themes.

--- src/my-theme-looper.el
+++ src/my-theme-looper.el
@@ -3,16 +3,14 @@
 (require 'theme-looper)
 
 (theme-looper-set-favorite-themes '(
-                                    base16-grayscale-dark ; first in list to cleanse palette
+                                    zerodark ; favorite
+                                    atom-dark
+                                    atom-one-dark
+                                    darcula
+                                    spacemacs-dark
+                                    ))
 
-                                    base16-chalk
-                                    idea-darkula
-                                    niflheim
-                                    noctilux
-                                    zerodark
-                              ))
-
-(global-set-key (kbd "<M-f12>")   'theme-looper-enable-next-theme) ; Mac
+(global-set-key (kbd "<M-f11>")   'theme-looper-enable-previous-theme)
+(global-set-key (kbd "<M-f12>")   'theme-looper-enable-next-theme)
 
 (provide 'my-theme-looper)
-
