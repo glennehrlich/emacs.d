63047640f981d6e24a4d1ab6144562372e432b1b
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-06-05 09:46:52 -0700

    Make tramp not cache connections because it does not work with containers.

--- custom.el
+++ custom.el
@@ -80,7 +80,7 @@
  '(smex-save-file "~/.emacs.d.persistent/smex/smex-items")
  '(sp-show-pair-from-inside t)
  '(split-height-threshold nil)
- '(tramp-persistency-file-name "~/.emacs.d.persistent/tramp/tramp")
+ '(tramp-persistency-file-name nil)
  '(transient-history-file "~/.emacs.d.transient/history.el")
  '(url-configuration-directory "~/.emacs.d.persistent/url/")
  '(vc-annotate-background nil)
