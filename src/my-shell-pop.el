510c0c5c7226dff2dd62a324ecab193997327e9b
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-06-06 14:28:43 -0700

    Change shell type to eshell in shell-pop-shell-type in my-shell-pop.el.

--- src/my-shell-pop.el
+++ src/my-shell-pop.el
@@ -5,7 +5,8 @@
 ;; Recent emacs have changed how shell works, this fixes old code that
 ;; expects shell to switch to the buffer. See src/my-local.el
 ;; shell-with-name that has a similar fix.
-(customize-set-variable 'shell-pop-shell-type '("shell" "*shell*" (lambda () (switch-to-buffer (shell)))))
+;; (customize-set-variable 'shell-pop-shell-type '("shell" "*shell*" (lambda () (switch-to-buffer (shell)))))
+(customize-set-variable 'shell-pop-shell-type '("eshell" "*eshell*" (lambda () (switch-to-buffer (eshell)))))
 
 (provide 'my-shell-pop)
 
