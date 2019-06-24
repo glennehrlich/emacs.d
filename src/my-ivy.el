fed2d0d85519446066d6c5893f5acb541e82d258
Glenn Ehrlich <glenn.ehrlich@gmail.com>
2019-06-24 13:58:20 -0700

    Added some more counsel functions.

--- src/my-ivy.el
+++ src/my-ivy.el
@@ -11,24 +11,24 @@
 ;; counsel-M-x will load it.
 (customize-set-variable 'smex-save-file "~/.emacs.d.persistent/smex/smex-items")
 
-(customize-set-variable 'counsel-ag-base-command "ag --hidden --ignore .git --nocolor --nogroup %s")
 (customize-set-variable 'counsel-find-file-at-point t)
 (customize-set-variable 'counsel-rg-base-command "rg --hidden --glob !.git -S --no-heading --line-number --color never %s .")
 (customize-set-variable 'ivy-count-format "%d/%d ")
 (customize-set-variable 'ivy-use-selectable-prompt t)
 
+(setq ivy-re-builders-alist
+      '((counsel-M-x . ivy--regex-fuzzy)
+        (t . ivy--regex-plus)))
+
+(global-set-key (kbd "<f8>")    'counsel-rg)
 (global-set-key (kbd "C-h a")   'counsel-apropos)
 (global-set-key (kbd "C-h f")   'counsel-describe-function)
 (global-set-key (kbd "C-h v")   'counsel-describe-variable)
 (global-set-key (kbd "C-s")     'counsel-grep-or-swiper)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x b")   'counsel-switch-buffer)
+(global-set-key (kbd "C-x m")   'counsel-compile)
 (global-set-key (kbd "M-x")     'counsel-M-x)
-(global-set-key (kbd "<f8>")    'counsel-rg)
-(global-set-key (kbd "<C-f8>")  'counsel-ag)
-
-(setq ivy-re-builders-alist
-      '((counsel-M-x . ivy--regex-fuzzy)
-        (t . ivy--regex-plus)))
 
 (provide 'my-ivy)
 
--- src/my-key-bindings.el
+++ src/my-key-bindings.el
@@ -52,8 +52,8 @@
 (define-key minibuffer-local-map (kbd "<f8>") 'next-history-element)
 
 ;; Other commands.
-;(global-set-key (kbd "M-g")     'goto-line)
-(global-set-key (kbd "C-x m")   'compile)
+;; (global-set-key (kbd "M-g")     'goto-line)
+;; (global-set-key (kbd "C-x m")   'compile)
 ;; (global-set-key (kbd "C-t")     'shell-pop)
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window) ; was list-buffers
 (global-set-key (kbd "C-=")     'er/expand-region)
