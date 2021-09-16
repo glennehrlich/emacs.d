12bc4eeb49400afd74586ee500430c04fe86a023
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2021-09-16 12:52:46 -0700

    Make tramp (and remote magit) use remote $PATH

--- src/my-tramp.el
+++ src/my-tramp.el
@@ -4,5 +4,9 @@
 
 (customize-set-variable 'tramp-persistency-file-name "~/.emacs.d.persistent/tramp/tramp")
 
+;; Use the remote user's PATH. See
+;; https://www.gnu.org/software/emacs/manual/html_node/tramp/Remote-programs.html
+(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
 (provide 'my-tramp)
 
