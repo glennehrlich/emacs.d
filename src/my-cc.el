cae59bec359692bb9ff966411e506b87d45758b2
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2021-11-19 16:01:40 -0700

    Add compile-cmake to my-cc.el

--- src/my-cc.el
+++ src/my-cc.el
@@ -3,6 +3,7 @@
 (require 'cc-mode)
 (require 'ggtags) ;; further customization of ggtags is in my-ggtags
 (require 'wrap-region)
+(require 'magit-git)
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
@@ -24,6 +25,7 @@
 (define-key c-mode-base-map (kbd "TAB") 'c-indent-line-or-region) ;; until auto complete figured out
 (define-key c-mode-base-map (kbd "S-<tab>") 'c-indent-line-or-region)
 (define-key c-mode-base-map (kbd "<backtab>") 'c-indent-line-or-region)
+(define-key c-mode-base-map (kbd "C-x m") 'compile-cmake)
 
 (add-hook 'c-mode-common-hook
           (lambda ()
@@ -31,5 +33,17 @@
             (wrap-region-mode 1)
             ))
 
+(defun compile-cmake (test-name)
+  "Make all and run all tests in the current git repo's build directory. If TEST-NAME is provide, make TEST-NAME and run it."
+  (interactive "sTest name: ")
+  (let ((build-dir (concat (magit-toplevel) "build"))
+        (command))
+    (unless (file-directory-p build-dir)
+      (error "No such directory: %s" build-dir))
+    (if (string-empty-p test-name)
+        (setq command (format "cd %s/ ; make && ctest --verbose" build-dir))
+      (setq command (format "cd %s/ ; make %s && ctest --verbose -R %s" build-dir test-name test-name)))
+    (compile command)))
+  
 (provide 'my-cc)
 
