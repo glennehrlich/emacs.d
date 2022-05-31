ab65539d01a2ebe04838424080f79adf4b7c8a4a
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2022-05-31 16:58:30 -0700

    my-magit.el changes: remove obsolete vars; add push remote columns to repo list

--- src/my-magit.el
+++ src/my-magit.el
@@ -3,12 +3,6 @@
 (require 'magit)
 (require 'magit-gitflow)
 
-(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
-
-(customize-set-variable 'magit-log-arguments '("--graph" "--color" "-n256"))
-
-(setq magit-completing-read-function 'ivy-completing-read)
-
 ;; Add git repos to be tracked with magit-list-repositories.
 (defun add-to-magit-repository-directories (dir-depth)
   (when (file-directory-p (car dir-depth)) ; only add if directory exists
@@ -25,11 +19,13 @@
 
 (customize-set-variable 'magit-repolist-columns
                         '(
-                          ("S"        1 magit-repolist-column-flag nil)
-                          ("↓"        1 magit-repolist-column-unpulled-from-upstream ((:help-echo "Upstream changes not in branch")))
-                          ("↑"        1 magit-repolist-column-unpushed-to-upstream ((:help-echo "Local changes not in upstream")))
-                          ("Branch"  25 magit-repolist-column-branch nil)
-                          ("Path"    99 magit-repolist-column-path nil)
+                          ("S"       1 magit-repolist-column-flag nil)
+                          ("U↓"      2 magit-repolist-column-unpulled-from-upstream nil)
+                          ("U↑"      2 magit-repolist-column-unpushed-to-upstream nil)
+                          ("P↓"      2 magit-repolist-column-unpulled-from-pushremote nil)
+                          ("P↑"      2 magit-repolist-column-unpushed-to-pushremote nil)
+                          ("Branch" 90 magit-repolist-column-branch nil)
+                          ("Path"   99 magit-repolist-column-path nil)
                           ))
 
 (customize-set-variable 'magit-repolist-column-flag-alist
