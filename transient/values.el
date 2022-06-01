afa62b42067a0ef36ec89b1ad1367d15e126248c
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2022-06-01 09:05:32 -0700

    Saved magit-fetch arg of --tags in transient/values.el

--- transient/values.el
+++ transient/values.el
@@ -1,2 +1,3 @@
-((magit-log:magit-log-mode "-n256" "--graph" "--color" "--decorate")
+((magit-fetch "--tags")
+ (magit-log:magit-log-mode "-n256" "--graph" "--color" "--decorate")
  (magit-merge "--no-ff"))
