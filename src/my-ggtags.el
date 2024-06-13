81d63f0cfc8d70ac2ba1c77562c04a3dddaf525b
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2024-06-13 16:08:54 -0700

    Added ggtags mode to org mode

--- src/my-ggtags.el
+++ src/my-ggtags.el
@@ -8,15 +8,16 @@
 (define-key ggtags-mode-prefix-map (kbd "M-g") nil)
 (define-key ggtags-navigation-map (kbd "M-o") nil)
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (ggtags-mode 1)
+(defun ggtags-mode-setup ()
+  (ggtags-mode 1)
 
-            ;; Remove ggtags mode keybindings that I'd rather it
-            ;; didn't override.
-            (keymap-unset ggtags-navigation-map "C-c M-g" t)
-            (keymap-unset ggtags-navigation-map "M-=" t)
-            ))
+  ;; Remove ggtags mode keybindings that I'd rather it
+  ;; didn't override.
+  (keymap-unset ggtags-navigation-map "C-c M-g" t)
+  (keymap-unset ggtags-navigation-map "M-=" t))
+
+(add-hook 'c-mode-common-hook 'ggtags-mode-setup)
+(add-hook 'org-mode-hook      'ggtags-mode-setup)
 
 (customize-set-variable 'ggtags-global-abbreviate-filename nil)
 
