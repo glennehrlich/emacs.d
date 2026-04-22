0a8dc1acb878d1b3b9cb3284fb2150608a2da58d
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2026-04-22 16:21:20 -0700

    Redo my-python.el for modern stuff

--- src/my-python.el
+++ src/my-python.el
@@ -1,42 +1,31 @@
 ;; -*- lexical-binding: t -*-
 ;;;; my-python.el - Customizations for python mode.
 
+(require 'corfu)
 (require 'eglot)
 (require 'flymake)
+(require 'jupyter-repl)
 (require 'python)
 
+;; Python modes hook.
+(defun my-python-hook ()
+  (eglot-ensure)
+  (setq corfu-auto t)                 ; turn on corfu for auto completion
+  (setq corfu-cycle t)                ; cycle through completion candidates with tab
+  (setq tab-always-indent 'complete)) ; tab key attempts smart completion or indentation
+(add-hook 'python-mode-hook 'my-python-hook)
+(add-hook 'python-ts-mode-hook 'my-python-hook)
 
-(add-hook 'python-ts-mode-hook #'eglot-ensure)
+(customize-set-variable 'python-indent-guess-indent-offset-verbose nil)
 
 (define-key python-ts-mode-map (kbd "<f2>") 'flymake-goto-prev-error)
 (define-key python-ts-mode-map (kbd "<f3>") 'flymake-goto-next-error)
 
-;; ;; glenn: disalbing this until I figure out what to do with it
-;; ;; (add-hook 'python-mode-hook 'importmagic-mode)
-
-
-;; ;; Always scroll to the end of the *Python* buffer.
-;; (add-hook 'inferior-python-mode-hook
-;;           (lambda ()
-;;             (setq comint-scroll-to-bottom-on-output t)))
-
-;; ;; Use ipthon.
-;; ;; (setq python-shell-interpreter "ipython"
-;; ;;       python-shell-interpreter-args "--simple-prompt -i")
-
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "--simple-prompt -c exec('__import__(\\'readline\\')') -i")
-
-;; ;; Use jupyter console.
-;; ;; (setq python-shell-interpreter "jupyter"
-;; ;;       python-shell-interpreter-args "console --simple-prompt"
-;; ;;       python-shell-prompt-detect-failure-warning nil)
-;; ;; (add-to-list 'python-shell-completion-native-disabled-interpreters
-;; ;;              "jupyter")
-
-;; ;; Turn on elpy.
-;; ;; glenn: 2022-11-16 turning elpy off because it is annoying when looking at random python code
-;; ;; (elpy-enable)
+;; Make the jupyter repl fonts more readable when using dark
+;; background.
+(custom-set-faces
+ '(jupyter-repl-input-prompt ((t (:foreground "cyan"))))
+ '(jupyter-repl-output-prompt ((t (:foreground "yellow")))))
 
 ;; Insert an empty __init__.py into the current directory.
 (defun init-py ()
