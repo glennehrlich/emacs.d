7a76d3a3fdbc5610bf0be938cec0aeb7284151bc
Glenn Ehrlich <glenn.ehrich2@boeing.com>
2026-04-22 16:11:22 -0700

    updated with new python oriented packages

--- custom.el
+++ custom.el
@@ -68,20 +68,23 @@
  '(org-table-use-standard-references t)
  '(org-todo-keywords '((sequence "TODO" "WORKING" "|" "DONE" "CANCELLED")))
  '(package-selected-packages
-   '(ace-window async avy bash-completion cmake-font-lock cmake-mode
-                company counsel counsel-tramp default-text-scale
-                docker docker-api docker-cli docker-compose-mode
-                dockerfile-mode ein el-patch elisp-slime-nav elpy
-                exec-path-from-shell expand-region flx flycheck fuzzy
-                ggtags git-timemachine hi2 hungry-delete hydra
-                impatient-mode importmagic ivy ivy-hydra jedi jsonian
-                magit marcopolo markdown-mode maven-test-mode morlock
-                org org-plus-contrib ox-clip paradox paredit-menu
-                plantuml-mode restclient shell-pop
-                skewer-reload-stylesheets smartparens smex string-edit
-                swiper theme-looper try undo-tree unfill
-                unicode-math-input use-package visual-regexp vterm w3m
-                web-mode wgrep wgrep-ag wrap-region xterm-color))
+   '(ace-window async bash-completion cmake-font-lock
+                color-theme-sanityinc-solarized
+                color-theme-sanityinc-tomorrow company corfu
+                counsel-tramp default-text-scale diminish docker
+                docker-compose-mode dockerfile-mode doom-themes ein
+                el-patch elisp-slime-nav exec-path-from-shell
+                expand-region flx flycheck fuzzy ggtags
+                git-timemachine hi2 impatient-mode importmagic
+                ivy-hydra jedi jsonian jupyter magit markdown-mode
+                maven-test-mode morlock org org-contrib ox-clip
+                paradox paredit-menu plantuml-mode px restclient
+                shell-pop skewer-reload-stylesheets smartparens smex
+                solarized-theme spacemacs-theme theme-looper
+                treesit-auto try undo-tree unfill unicode-math-input
+                visual-regexp vterm-toggle w3m web-mode wgrep-ag
+                wrap-region xterm-color yasnippet zenburn-theme
+                zerodark-theme))
  '(paradox-github-token t)
  '(recentf-save-file "~/.emacs.d.persistent/recentf/recentf")
  '(request-storage-directory "~/.emacs.d.transient/request")
